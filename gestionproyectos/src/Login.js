ede1b2c08f2fb98287acc32a3b6f13226bef2850
nicolaspy <nicolasbolanos@uninorte.edu.co>
2021-12-04 12:31:28 -0500

    correr npm install otra vez

--- gestionproyectos/src/Login.js
+++ gestionproyectos/src/Login.js
@@ -52,4 +52,26 @@ const Login = () =>{
 }
 
 
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Colombia");
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            onChange={(e) => setLocation(e.target.value)}
+            value={location}
+            placeholder="Location"
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
 export default Login;
