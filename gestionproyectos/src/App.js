82120dcfd737cb7f264450aea53ad4f5088f3b0d
Ang3l1t0 <aepalma4712@gmail.com>
2021-12-04 20:17:03 -0500

    components

--- gestionproyectos/src/App.js
+++ gestionproyectos/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
+import User from "./components/User";
+import Projectinfo from "./components/Projectinfo";
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <Navbar />
+      <div>
+        <div className="container">
+          <Navbar />
+          <User />
+          <Projectinfo />
+        </div>
       </div>
     );
   }
