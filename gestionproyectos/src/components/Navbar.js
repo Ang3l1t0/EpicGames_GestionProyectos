95b05a3e63326113e0da136995e58e5738650b44
Ang3l1t0 <aepalma4712@gmail.com>
2021-12-04 19:33:49 -0500

    navbar

---
+++ gestionproyectos/src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm bg-primary navbar-dark">
+        <a className="navbar-brand" href="#">
+          Epic
+        </a>
+
+        <ul className="navbar-nav">
+          <li className="nav-item ">
+            <a className="nav-link" href="Home.html">
+              Inicio
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="Report.html">
+              Informe
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="CreateProject.html">
+              Crear Proyecto
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="">
+              Cerrar Sesión
+            </a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
